fb48d7fd5d13220b131aeba91a1e95932f51db76
Farhan <iryadifarhan@gmail.com>
2026-07-16 12:46:05 +0700

    Hotfix for the user seeder amount

--- database/seeders/TestUserSeeder.php
+++ database/seeders/TestUserSeeder.php
@@ -24,7 +24,7 @@ class TestUserSeeder extends Seeder
  
     private function seedPatients(): void
     {
-        for ($i = 1; $i <= 100; $i++) {
+        for ($i = 1; $i <= 50; $i++) {
             User::query()->firstOrCreate([
                 'email' => "patient_{$i}@testing.com",
                 'username' => "patient_{$i}",
